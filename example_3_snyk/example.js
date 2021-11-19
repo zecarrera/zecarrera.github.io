752d04299c70eeddcdf59c5da0c0a946a5f0099c
Jose Neto <jose.neto@bglgroup.co.uk>
2021-11-19 10:25:14 +0000

    Adds some example changes

---
+++ example_3_snyk/example.js
@@ -0,0 +1,10 @@
+module.exports = function () {
+  const user = {
+    name: "username",
+    age: 30,
+    address: "example street",
+    postcode: "pee123"
+  }
+  user.lastname = 'lastname'
+  return user
+}
